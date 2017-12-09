f3ebe22b8148a40a6a018306a44f4b5f260fd3a2
superbuggy <jpqreichard@gmail.com>
2017-12-09 10:55:25 -0500

    add Logo svg experiment

---
+++ src/components/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = () => (  
+  <svg>
+    <polygon points="0,0 50,0 50,50 125,150" />
+    <polygon points="250,0 200,0 200,50 125,150" />
+    <polygon points="0,300 50,300 50,250 125,150" />
+  </svg>
+)
+
+export default Logo
