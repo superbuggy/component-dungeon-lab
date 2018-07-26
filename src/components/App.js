bc17d8cc9203f1cde53600231537d896317cf172
James Reichard <jpqreichard@gmail.com>
2018-07-26 19:58:33 -0400

    Import piano svg into dummy component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import Knob from './Knob'
+import Piano from './Piano'
 import Wheel from './Wheel/Wheel'
 
 class App extends Component {
   render () {
     return (
       <div>
-        <Wheel slices={5} />
-        <Knob />
+        {/* <Wheel slices={5} /> */}
+        {/* <Knob /> */}
+        <Piano />
       </div>
     )
   }
